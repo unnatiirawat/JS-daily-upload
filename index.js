ccd3ae15fdf551210007ba2361ba94495a0b7003
unnatiirawat <140919232+unnatiirawat@users.noreply.github.com>
2025-02-20 10:40:26 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,63 @@
+console.log("Hello World");
+
+var num = 20;
+var  num = 30;
+console.log(num);
+// can be redeclare and rename.
+
+let num1 = 50;
+ num1 = 23
+console.log(num1);
+// can reassign and cannot redeclare
+
+const num2 = 80;
+// num2 = 32;
+console.log(num2);
+
+
+// // keywords
+// let
+// var
+// const
+
+
+//operators ;- use to perform diferent operations
+
+console.log("hello");
+
+// let a = 20;
+// let b = 16;
+// let res = b%a;
+
+// console.log(res);
+
+//comparative operators ;- (==), (===), (!=), (!==)
+// let a1 = 20;
+// let b2 = true;
+// let res = b2 !=a1;
+// console.log(res);
+
+// let a = 20;
+// let b = true;
+// let res2 =  b!==a;
+// console.log(res2);
+
+// let a3= 20;
+// let b3 = 50;
+// let res3 =  a3<=b3;
+// console.log(res3);
+
+// let x= 2;
+// let y = 5;
+// let res1 = x+y;
+// console.log(res1);
+
+let t = 4567;
+let result = Math.floor(t/1000);
+console.log(result);
+
+
+
+
+
+
